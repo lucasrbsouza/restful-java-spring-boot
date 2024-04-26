c52db73cdaa865e5bd6fc1fe079a889258114799
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2024-04-26 11:15:26 -0300

    Initial commit

---
+++ java-with-spring/src/main/java/br/com/java/spring/javawithspring/JavaWithSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.spring.javawithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWithSpringApplication.class, args);
+	}
+
+}
---
+++ java-with-spring/src/test/java/br/com/java/spring/javawithspring/JavaWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.spring.javawithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
